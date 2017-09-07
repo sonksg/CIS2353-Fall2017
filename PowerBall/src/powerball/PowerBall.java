ac3d965a19cdd90ce2dec695d551b4f536a3139f
Eric Charnesky <eric.charnesky@careevolution.com>
2017-09-07 19:12:24 -0400

    PowerBall complete

--- PowerBall/src/powerball/PowerBall.java
+++ PowerBall/src/powerball/PowerBall.java
@@ -5,17 +5,110 @@
  */
 package powerball;
 
+import java.util.Random;
+
 /**
  *
  * @author etcharn1
  */
 public class PowerBall {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static final int TICKET_PRICE = 2;
+    
+    private int[] whiteBalls;
+    private int redBall;
+    
+    public PowerBall()
+    {
+        Random random = new Random();
+        whiteBalls = new int[5];
+        redBall = random.nextInt(26) + 1;
+        
+        int[] bucket = new int[69];
+        for( int number = 0; number < bucket.length; number++)
+        {
+            bucket[number] = number+1;
+        }
+        
+        for ( int whiteBallIndex = 0; whiteBallIndex < whiteBalls.length; whiteBallIndex++ )
+        {
+            int randomBall = random.nextInt( bucket.length );
+            while ( bucket[randomBall] == 0 )
+            {
+                randomBall = random.nextInt( bucket.length );
+            }
+            
+            whiteBalls[whiteBallIndex] =  bucket[randomBall];
+            bucket[randomBall] = 0;
+        }
     }
     
+    public PowerBall(int[] whiteBalls, int redBall)
+    {
+        // TODO - this is bad - we should check for dupes
+        this.whiteBalls = whiteBalls;
+        this.redBall = redBall;
+    }
+    
+    public int getWinnings( PowerBall winningTicket )
+    {
+        int winnings = 0;
+        
+        boolean redMatches = winningTicket.redBall == this.redBall;
+        
+        int whiteMatches = 0;
+        
+        for ( int myWhiteBall : whiteBalls )
+        {
+            for ( int winningWhiteBall : winningTicket.whiteBalls )
+            {
+                if (myWhiteBall == winningWhiteBall)
+                {
+                    whiteMatches++;
+                }
+            }
+        }
+        
+        if ( whiteMatches == 5 && redMatches )
+        {
+            winnings = 1000000000;
+        }
+        else if ( whiteMatches == 5 )
+        {
+            winnings = 1000000;
+        }
+        else if ( whiteMatches == 4 && redMatches )
+        {
+            winnings = 50000;
+        }
+        else if ( whiteMatches == 4 || ( whiteMatches == 3 && redMatches ) )
+        {
+            winnings = 100;
+        }
+        else if ( whiteMatches == 3 || ( whiteMatches ==2 && redMatches ) )
+        {
+            winnings = 7;
+        }
+        else if ( redMatches )
+        {
+            winnings = 4;
+        }
+        
+        return winnings;
+    }
+    
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+        for ( int whiteBall : whiteBalls )
+        {
+            builder.append(whiteBall);
+            builder.append(" ");
+        }
+        builder.append("PowerBall: ");
+        builder.append( redBall );
+        
+        return builder.toString();
+    }
+
 }
---
+++ PowerBall/src/powerball/PowerBallSimulator.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package powerball;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author etcharn1
+ */
+public class PowerBallSimulator {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int totalSpent = 0;
+        int totalWon = 0;
+        
+        int numberOfTicketsToPurchase = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                "Please enter the number of tickets you want to buy, or 0 to quit") );
+        
+        while ( numberOfTicketsToPurchase != 0 )
+        {
+            totalSpent += numberOfTicketsToPurchase * PowerBall.TICKET_PRICE;
+            PowerBall winningTicket = new PowerBall();
+            
+            for ( int ticketNumber = 0; ticketNumber < numberOfTicketsToPurchase; ticketNumber++ )
+            {
+                PowerBall myTicket = new PowerBall();
+                totalWon += myTicket.getWinnings( winningTicket );
+                if ( myTicket.getWinnings(winningTicket ) == 1000000000 )
+                {
+                    JOptionPane.showMessageDialog(null, "JACKPOT!!!");
+                }
+            }
+            String message = String.format("Total Spent: %d - Total Won: %d - Net Loss: %d", 
+                    totalSpent, totalWon, totalWon - totalSpent );
+            JOptionPane.showMessageDialog(null, message);
+            
+            numberOfTicketsToPurchase = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                "Please enter the number of tickets you want to buy, or 0 to quit") );
+        }
+    }
+}
